aabd71f1697bbbd976b89b42aed8f46bd81bc2ad
Emmanuel <imanuelchibuzor@gmail.com>
2025-10-06 21:56:37 +0100

    completed the sidebar ui

--- user/client/src/layouts/Sidebar.tsx
+++ user/client/src/layouts/Sidebar.tsx
@@ -1,18 +1,80 @@
+import { Link } from "react-router-dom";
+import {
+  Bell,
+  ChartColumnIncreasing,
+  CircleCheckBig,
+  House,
+  Library,
+  Loader2,
+  LogOut,
+  Menu,
+  Share2,
+  ShoppingBag,
+  SquarePen,
+  Upload,
+  User,
+  Wallet,
+  X,
+} from "lucide-react";
+
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
   SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import { Menu } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 const Sidebar = () => {
+  const merchant = true;
+  const user = {
+    name: "John Doe",
+    avatar:
+      "https://images.unsplash.com/photo-1499714608240-22fc6ad53fb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=76&q=80",
+  };
+  const image = false;
+  const loading = false;
+  const failed = false;
+  const success = false;
+
+  const navItems = [
+    { path: "/", icon: <House className="size-5" />, label: "Home" },
+    {
+      path: "/library",
+      icon: <Library className="size-5" />,
+      label: "Library",
+    },
+    !merchant && {
+      path: "/merchant",
+      icon: <ChartColumnIncreasing className="size-5" />,
+      label: "Become a Merchant",
+    },
+    merchant && {
+      path: "/notification",
+      icon: <Bell className="size-5" />,
+      label: "Notification",
+    },
+    merchant && {
+      path: "/vendor",
+      icon: <ShoppingBag className="size-5" />,
+      label: "Vendor",
+    },
+    merchant && {
+      path: "/affiliate",
+      icon: <Share2 className="size-5" />,
+      label: "Affiliate",
+    },
+    merchant && {
+      path: "/wallet",
+      icon: <Wallet className="size-5" />,
+      label: "Wallet",
+    },
+  ].filter((item) => typeof item === "object" && item !== null);
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -20,25 +82,72 @@ const Sidebar = () => {
           <Menu />
         </Button>
       </SheetTrigger>
-      <SheetContent side="left">
+      <SheetContent side="left" className="">
         <SheetHeader>
-          <SheetTitle>Edit profile</SheetTitle>
-          <SheetDescription>
-            Make changes to your profile here. Click save when you&apos;re done.
-          </SheetDescription>
+          <SheetTitle>{user.name}</SheetTitle>
         </SheetHeader>
-        <div className="grid flex-1 auto-rows-min gap-6 px-4">
-          <div className="grid gap-3">
-            <Input id="sheet-demo-name" defaultValue="Pedro Duarte" />
-          </div>
-          <div className="grid gap-3">
-            <Input id="sheet-demo-username" defaultValue="@peduarte" />
-          </div>
+        {user && (
+          <>
+            <div className="flex items-center justify-center p-1 gap-2 rounded-full">
+              {user.avatar ? (
+                <img
+                  src={user.avatar}
+                  alt={user.name}
+                  className="h-20 w-20 rounded-full object-cover"
+                />
+              ) : (
+                <User className="h-8 w-8 border rounded-full p-1" />
+              )}
+            </div>
+            <div className="flex items-center justify-center text-muted-foreground">
+              {!image && !loading && !failed && !success && (
+                <Button variant="outline">
+                  <SquarePen />
+                </Button>
+              )}
+              {image && !loading && !failed && !success && (
+                <Button variant="outline">
+                  <Upload className="text-blue-500" />
+                </Button>
+              )}
+              {image && loading && !failed && !success && (
+                <Button variant="outline">
+                  <Loader2 className="animate-spin" />
+                </Button>
+              )}
+              {!image && !loading && failed && !success && (
+                <Button variant="outline">
+                  <X className="text-primary" />
+                </Button>
+              )}
+              {!image && !loading && !failed && success && (
+                <Button variant="outline">
+                  <CircleCheckBig className="text-green-500" />
+                </Button>
+              )}
+            </div>
+          </>
+        )}
+        <Separator />
+
+        <div className="grid flex-1 auto-rows-min gap-3 px-4">
+          {navItems.map(({ path, icon, label }) => (
+            <SheetClose
+              asChild
+              className="flex items-center gap-3 rounded-lg px-4 py-2.5 text-sm font-medium transition-all duration-200 ease-in-out hover:bg-primary/30"
+            >
+              <Link key={path} to={path}>
+                {icon}
+                <span>{label}</span>
+              </Link>
+            </SheetClose>
+          ))}
         </div>
         <SheetFooter>
-          <Button type="submit">Save changes</Button>
           <SheetClose asChild>
-            <Button variant="outline">Close</Button>
+            <Button variant="outline">
+              <LogOut /> Logout
+            </Button>
           </SheetClose>
         </SheetFooter>
       </SheetContent>
--- user/client/src/pages/NotFound.tsx
+++ user/client/src/pages/NotFound.tsx
@@ -1,13 +1,21 @@
-import { Button } from "@/components/ui/button";
 import { TriangleAlert } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
+import { Button } from "@/components/ui/button";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="flex flex-col items-center justify-center gap-3 mt-5">
       <TriangleAlert className="w-20 h-20 text-primary" />
       <h1 className="font-semibold text-xl">404</h1>
       <p className="">Sorry, this page does not exist</p>
-      <Button variant="outline" className="hover:bg-rose-100">
+      <Button
+        variant="outline"
+        className="hover:bg-rose-100"
+        onClick={() => navigate("/")}
+      >
         Go Home
       </Button>
     </div>
